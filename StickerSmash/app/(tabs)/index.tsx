40bcfe72b99e57b4d5618176fda6fd294e564e35
Faraji Ombonya <farajiombonya@gmail.com>
2024-11-16 09:48:11 +0300

    use an image picker

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,4 +1,6 @@
 import { View, StyleSheet } from "react-native";
+import { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
@@ -6,13 +8,33 @@ import ImageViewer from "@/components/ImageViewer";
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image!");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme="primary" />
+        <Button label="Choose a photo" theme="primary" onPress={pickImage} />
         <Button label="Use this photo" />
       </View>
     </View>
