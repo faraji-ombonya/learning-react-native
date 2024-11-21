b54b82dbc178d73dbdf5e62aa11cb7692b6647d4
Faraji Ombonya <farajiombonya@gmail.com>
2024-11-21 12:01:18 +0300

    configure status bar, splash screen and app icon

--- StickerSmash/app/_layout.tsx
+++ StickerSmash/app/_layout.tsx
@@ -1,6 +1,14 @@
 import { Stack } from "expo-router";
+import { setStatusBarStyle } from "expo-status-bar";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  useEffect(() => {
+    setTimeout(() => {
+      setStatusBarStyle("dark");
+    }, 0);
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
